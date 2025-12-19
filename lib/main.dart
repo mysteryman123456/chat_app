482c2cce283211ed2368ec08ddb0303be1058e2f
SiteDecors <decorssite@gmail.com>
2025-12-19 12:25:37 +0545

    Changed theme mode and added manrope in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:chat_app/screens/on_boarding_screen/first_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(ChatApp());
+void main() {
+  runApp(const ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
@@ -11,8 +11,18 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: FirstScreen(),
+      theme: ThemeData(fontFamily: "Manrope"),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: ThemeData.dark().textTheme.apply(
+          fontFamily: "Manrope",
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        ),
+      ),
+
+      home: const FirstScreen(),
     );
   }
 }
