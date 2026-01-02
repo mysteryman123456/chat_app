284bb1cdd4e9f03778b393cf9cd175247184ac8f
SiteDecors <decorssite@gmail.com>
2026-01-02 22:12:50 +0545

    Added notification

---
+++ lib/features/notification/presentation/pages/notification_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(Icons.notifications_none, size: 80, color: Colors.white),
+          SizedBox(height: 16),
+          Text(
+            "Notifications will appear here",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
