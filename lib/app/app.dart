7b38e8acd353cbd19a22dc4f160dd6c3cda7bc01
SiteDecors <decorssite@gmail.com>
2026-01-02 14:01:48 +0545

    Added folder structure

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:chat_app/app/theme/theme_data.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+      home: Placeholder(),
+    );
+  }
+}
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import 'login_screen.dart';
@@ -12,35 +10,76 @@ class SignupScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.all(40),
+        child: Padding(
+          padding: EdgeInsets.all(40),
           child: Container(
             width: double.infinity,
             alignment: Alignment.center,
             child: Column(
               children: [
-                Text("Create an account", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                SizedBox(height: 10,),
+                Text(
+                  "Create an account",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(height: 10),
                 Text("Start your messaging journey with GinChat"),
-                SizedBox(height: 40,),
-                TextFormField(decoration: InputDecoration(labelText: "Username",border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),borderRadius: BorderRadius.all(Radius.circular(10))))),
-                SizedBox(height:20,),
-                TextFormField(decoration: InputDecoration(labelText: "Password",border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),borderRadius: BorderRadius.all(Radius.circular(10))))),
-                SizedBox(height:30,),
-                SizedBox(width: double.infinity,child: ElevatedButton(style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15),backgroundColor: Colors.indigo,shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))) ,onPressed: (){}, child: Text("Signup")),),
-                SizedBox(height:10,),
+                SizedBox(height: 40),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(15),
+                      backgroundColor: Colors.indigo,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text("Signup"),
+                  ),
+                ),
+                SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text("Already have an account?"),
                     TextButton(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (_) => LoginScreen()),
+                        );
                       },
-                      child: Text("Login",style: TextStyle(color: Colors.white)),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ],
-                )
-              ],),
+                ),
+              ],
+            ),
           ),
         ),
       ),
