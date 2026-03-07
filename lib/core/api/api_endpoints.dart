a73eae38047c34d30622f9dc076936e5ead16c55
mysteryman123456 <nschldhl61@gmail.com>
2026-03-07 16:29:07 +0545

    add forgot and reset password endpoints

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
+  static const String baseUrl = 'http://192.168.254.12:8080/api';
   // For iOS Simulator use: 'http://localhost:5000/api/v1'
   // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api/v1'
 
@@ -12,4 +12,18 @@ class ApiEndpoints {
   static const String login = '/auth/login';
   static const String register = '/auth/signup';
   static const String getAllUsers = '/auth/users';
+  static const String updateProfile = '/user/profile';
+  static const String updatePassword = '/user/settings/password';
+  static const String forgotPassword = '/auth/forgot-password';
+  static const String resetPassword = '/auth/reset-password';
+  
+  // Conversations
+  static const String getConversations = '/conversation';
+  static const String createConversation = '/conversation';
+
+  static const String getMessages = '/message';
+
+  static const String searchUser = '/user';
+
+  static const String uploadImage = '/upload/file';
 }
